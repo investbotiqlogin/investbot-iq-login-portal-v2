912012de1830d7d7648a4c6d824d224d05aad227
Investbotiq <investbotiq@gmail.com>
2025-05-01 17:01:43 +0200

    Remove NavBar header by rendering nothing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,9 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from './Logo';
 
 const NavBar = () => {
-  return (
-    <nav className="w-full py-4 px-6 flex justify-between items-center bg-background/80 backdrop-blur-md z-10">
-      <Link to="/">
-        <img src="/investbot-logo.png" alt="InvestbotIQ Logo" width={48} height={48} style={{ display: 'inline-block' }} />
-      </Link>
-      
-      <Link
-        to="/inlog"
-        className="px-6 py-2 bg-investbot-purple text-white rounded-md hover:bg-investbot-darkPurple transition-colors"
-      >
-        Inloggen
-      </Link>
-    </nav>
-  );
+  return null;
 };
 
 export default NavBar;
